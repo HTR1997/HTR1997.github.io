3d471c759f8565ec3d1829071e6cb7c561416dc4
Harrison Read <harrison_thoreau@mac.com>
2021-07-27 13:27:10 -0400

    Added circles page to site.
    First page after the introduction. Tried a bunch of ideas for the design
    but am going to use a circle with radius. The radius can be turned by the
    user.

---
+++ content/circles.js
@@ -0,0 +1,55 @@
+let canvas;
+let DIAMETER, RADIUS;
+const SHIFT_VALUES = [1/4, 3/8];
+const BACKGROUND_COLOR = 236;
+
+function setup(){
+  canvas = createCanvas(800, 800)
+  canvas.parent('sketch-holder');
+  p5.disableFriendlyErrors = true; 
+  strokeWeight(3);
+  noFill();
+  noCursor();
+
+  //Current challenge is to find good sizes CAN BE TWEAKED LATER
+  resizeCanvas(windowWidth, windowHeight);
+  if (windowWidth < windowHeight){
+    DIAMETER = windowWidth/2;
+  } else {
+    DIAMETER = windowHeight/2;
+  }
+  RADIUS = DIAMETER/2;
+}
+
+//Main loop 
+function draw(){
+  if (focused){
+    background(BACKGROUND_COLOR);
+    circle(mouseX, mouseY, 20);//Cursor
+    translate(windowWidth*SHIFT_VALUES[0], windowHeight*SHIFT_VALUES[1]);
+
+    angle = atan2(mouseY-windowHeight*SHIFT_VALUES[1],
+      mouseX-windowWidth*SHIFT_VALUES[0]);
+
+    circle(0,0, DIAMETER);
+    push();
+    stroke([44, 200, 160]);
+    strokeWeight(4);
+    //arc(0, 0, DIAMETER, DIAMETER, angle , 0);
+    
+    fill([44, 200, 160]);
+    circle(RADIUS*cos(angle), RADIUS*sin(angle), 8);
+    //circle(RADIUS, 0, 8);
+    circle(0, 0, 8);
+    line(0,0, RADIUS*cos(angle), RADIUS*sin(angle));
+    pop();
+  } else {
+    //Do nothing
+    //May reduce cpu usage when not the focused page. 
+  }
+}
+
+
+
+
+
